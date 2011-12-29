4587cb69b8ca95d93d6565de7696af85797ade84
Stéphan Gorget <stephan.gorget@edf.fr>
2011-12-29 10:54:51 +0000

    definissions du model et base de donnes pour les jobs

--- lib/HPCStats/Model/Job.py
+++ lib/HPCStats/Model/Job.py
@@ -2,33 +2,30 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
-from HPCStats.DB.Jobs import DBJobs
 
 class Job:
 
-    def __init__(self, nb =0):
-        self.number = nb
-        self.user = ""
-        self.groups = []
-        self.submission_datetime = 0
-        self.running_datetime = 0
-        self.end_datetime = 0
-        self.nb_procs = 0
-        self.nb_hosts = 0
-        self.running_queue = ""
-        self.hosts = []
+    def __init__(self, id_job = 0):
+        self._id_job = id_job
+        self._uid = ""
+        self._submission_datetime = 0
+        self._running_datetime = 0
+        self._end_datetime = 0
+        self._nb_procs = 0
+        self._nb_hosts = 0
+        self._running_queue = ""
+        self._nodes = ""
 
     def __str__(self):
-        if self.running_datetime == 0:
+        if self._running_datetime == 0:
            running_datetime = "notyet"
         else:
-           running_datetime = self.running_datetime.strftime('%Y-%m-%d %H:%M:%S')
-        if self.end_datetime == 0:
+           running_datetime = self._running_datetime.strftime('%Y-%m-%d %H:%M:%S')
+        if self._end_datetime == 0:
            end_datetime = "notyet"
         else:
-           end_datetime = self.end_datetime.strftime('%Y-%m-%d %H:%M:%S')
-        return self.number + " (" + self.user + "): " + self.submission_datetime.strftime('%Y-%m-%d %H:%M:%S') + " / " + running_datetime + " / " + end_datetime + " -> " + str(self.nb_hosts) + "/"  + str(self.nb_procs) + " [" + ",".join(self.hosts) + "]"
+           end_datetime = self._end_datetime.strftime('%Y-%m-%d %H:%M:%S')
+        return self._id_job + " (" + self._uid + "): " + self._submission_datetime.strftime('%Y-%m-%d %H:%M:%S') + " / " + running_datetime + " / " + end_datetime + " -> " + str(self._nb_hosts) + "/"  + str(self._nb_procs) + " [" + ",".self._nodes + "]"
 
-    def store(self, db):
-        dbjob = DBJobs(db)
-        dbjob.store(self)
+    def jobs_query(self):
+        return 'INSERT INTO jobs_tmp (id_job, uid, running_queue, submission_datetime, running_datetime, end_datetime, nb_nodes, nb_cpus) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)', (self._id_job, self._uid, self._running_queue, self._submission_datetime, self._running_datetime, self._end_datetime, self._nb_procs, self._nb_hosts)
