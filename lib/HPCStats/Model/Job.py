87b4737d5cf68d7ad682f36c30c0424f58f65ca3
Rémi Palancher <remi.palancher@edf.fr>
2012-01-05 13:43:35 +0000

    hpcstats: code cleaning in job model

--- lib/HPCStats/Model/Job.py
+++ lib/HPCStats/Model/Job.py
@@ -5,7 +5,21 @@ from datetime import datetime
 
 class Job:
 
-    def __init__(self, db_id = 0, id_job = 0, sched_id = 0, clustername = "", uid = -1, gid = -1, submission_datetime = 0, running_datetime = 0, end_datetime = 0, nb_procs = 0, nb_hosts = 0, running_queue = "", nodes = "", state = "unknown"):
+    def __init__( self,
+                  db_id = 0,
+                  id_job = 0,
+                  sched_id = 0,
+                  clustername = "",
+                  uid = -1,
+                  gid = -1,
+                  submission_datetime = 0,
+                  running_datetime = 0,
+                  end_datetime = 0,
+                  nb_procs = 0,
+                  nb_hosts = 0,
+                  running_queue = "",
+                  nodes = "",
+                  state = "unknown" ):
         self._db_id = db_id
         self._sched_id = sched_id
         self._id_job = id_job
@@ -33,20 +47,67 @@ class Job:
         return self._id_job + " (" + self._uid+"|"+self._gid + "): " + self._submission_datetime.strftime('%Y-%m-%d %H:%M:%S') + " / " + self._running_datetime.strftime('%Y-%m-%d %H:%M:%S') + " / " + self._end_datetime.strftime('%Y-%m-%d %H:%M:%S') + " -> " + str(self._nb_hosts) + "/"  + str(self._nb_procs) + " [" + self._nodes + "]"+ self._state
 
     def save(self, db):
-        req = "INSERT INTO jobs (id_job, sched_id, uid, gid, clustername, running_queue, submission_datetime, running_datetime, end_datetime, nb_nodes, nb_cpus, state) VALUES ('%s', '%s', %s, %s, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (self._id_job, self._sched_id, self._uid, self._gid, self._clustername, self._running_queue, self._submission_datetime.strftime('%Y-%m-%d %H:%M:%S'), self._running_datetime.strftime('%Y-%m-%d %H:%M:%S'), self._end_datetime.strftime('%Y-%m-%d %H:%M:%S'), self._nb_hosts, self._nb_procs, self._state)
-        db.get_cur().execute(req)
+        req = """
+            INSERT INTO jobs (
+                            id_job,
+                            sched_id,
+                            uid,
+                            gid,
+                            clustername,
+                            running_queue,
+                            submission_datetime,
+                            running_datetime,
+                            end_datetime,
+                            nb_nodes,
+                            nb_cpus,
+                            state)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s); """
+        datas = (
+            self._id_job,
+            self._sched_id,
+            self._uid,
+            self._gid,
+            self._clustername,
+            self._running_queue,
+            self._submission_datetime.strftime('%Y-%m-%d %H:%M:%S'),
+            self._running_datetime.strftime('%Y-%m-%d %H:%M:%S'),
+            self._end_datetime.strftime('%Y-%m-%d %H:%M:%S'),
+            self._nb_hosts,
+            self._nb_procs,
+            self._state )
+ 
+        #print db.get_cur().mogrify(req, datas)
+        db.get_cur().execute(req, datas)
         
     def update(self, db):
-        req = "UPDATE jobs SET \
-               id_job = '%s', \
-                uid = %s, \
-                gid = %s, \
-                clustername = '%s', \
-                running_queue = '%s', \
-                submission_datetime = '%s', \
-                running_datetime = '%s', \
-                end_datetime = '%s', \
-                nb_nodes = '%s', \
-                nb_cpus = '%s', \
-                state = '%s' WHERE sched_id = '%s' " % (self._id_job, self._uid, self._gid, self._clustername, self._running_queue, self._submission_datetime.strftime('%Y-%m-%d %H:%M:%S'), self._running_datetime.strftime('%Y-%m-%d %H:%M:%S'), self._end_datetime.strftime('%Y-%m-%d %H:%M:%S'), self._nb_hosts, self._nb_procs, self._state, self._sched_id)
-        db.get_cur().execute(req)
+        req = """
+            UPDATE jobs SET
+                       id_job = %s,
+                       uid = %s,
+                       gid = %s,
+                       clustername = %s,
+                       running_queue = %s,
+                       submission_datetime = %s,
+                       running_datetime = %s,
+                       end_datetime = %s,
+                       nb_nodes = %s,
+                       nb_cpus = %s,
+                       state = %s
+            WHERE sched_id = %s; """
+        datas = (
+            self._id_job,
+            self._uid,
+            self._gid,
+            self._clustername,
+            self._running_queue,
+            self._submission_datetime.strftime('%Y-%m-%d %H:%M:%S'),
+            self._running_datetime.strftime('%Y-%m-%d %H:%M:%S'),
+            self._end_datetime.strftime('%Y-%m-%d %H:%M:%S'),
+            self._nb_hosts,
+            self._nb_procs,
+            self._state,
+            self._sched_id )
+
+        #print db.get_cur().mogrify(req, datas)
+        db.get_cur().execute(req, datas)
+
