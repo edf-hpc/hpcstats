0582daa7a6313f7b0186f5443e67b3330df85ad9
Stéphan Gorget <stephan.gorget@edf.fr>
2012-02-17 13:08:34 +0000

    Update torque job importer

--- lib/HPCStats/Importer/Jobs/JobImporterTorque.py
+++ lib/HPCStats/Importer/Jobs/JobImporterTorque.py
@@ -2,28 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from HPCStats.Model.Job import Job
+from ClusterShell.NodeSet import NodeSet
 from datetime import datetime
-import re
+import re, os
 
 # 02/01/2012 18:56:28;E;5802977.clay1pno;user=martin group=lnhe jobname=2009_juil_dec queue=parall_64 ctime=1328114089 qtime=1328114089 etime=1328114089 start=1328114090 owner=martin@clay1pfr exec_host=cla1a184/7+cla1a184/6+cla1a184/5+cla1a184/4+cla1a184/3+cla1a184/2+cla1a184/1+cla1a184/0+cla1a185/7+cla1a185/6+cla1a185/5+cla1a185/4+cla1a185/3+cla1a185/2+cla1a185/1+cla1a185/0+cla1b003/7+cla1b003/6+cla1b003/5+cla1b003/4+cla1b003/3+cla1b003/2+cla1b003/1+cla1b003/0+cla1b004/7+cla1b004/6+cla1b004/5+cla1b004/4+cla1b004/3+cla1b004/2+cla1b004/1+cla1b004/0 Resource_List.neednodes=4:ppn=8 Resource_List.nodect=4 Resource_List.nodes=4:ppn=8 Resource_List.walltime=06:00:00 session=8351 end=1328118988 Exit_status=0 resources_used.cput=40:56:18 resources_used.mem=1270208kb resources_used.vmem=6968788kb resources_used.walltime=01:21:38
 
 # ('02/01/2012 18:56:28', '5802977', None, 'martin', None, 'lnhe', 'parall_64', '1328114089', '1328114089', '1328114090', 'cla1a184/7+cla1a184/6+cla1a184/5+cla1a184/4+cla1a184/3+cla1a184/2+cla1a184/1+cla1a184/0+cla1a185/7+cla1a185/6+cla1a185/5+cla1a185/4+cla1a185/3+cla1a185/2+cla1a185/1+cla1a185/0+cla1b003/7+cla1b003/6+cla1b003/5+cla1b003/4+cla1b003/3+cla1b003/2+cla1b003/1+cla1b003/0+cla1b004/7+cla1b004/6+cla1b004/5+cla1b004/4+cla1b004/3+cla1b004/2+cla1b004/1+cla1b004/0', '06:00:00')
-
-"""
-        job = Job(  id_job = tab[1]
-                    sched_id = file:number
-                    uid = uid(tab[3])
-                    gid = gid(tab[4])
-                    submission_datetime = datetime.fromtimestamp(tab[7]),
-                    running_datetime = datetime.fromtimestamp(tab[8]),
-                    end_datetime = datetime.fromtimestamp(tab[9]),
-                    nb_procs = calc()
-                    nb_hosts = calc()
-                    running_queue = tab[6]
-                    nodes = tab[10],
-                    state = "Exit
-                    clustername = self._cluster_name)
-"""
 
 class JobImporterTorque(object):
 
@@ -32,7 +17,8 @@ class JobImporterTorque(object):
         self._conf = config
         self._cluster_name = cluster_name
 
-        self._logpat = re.compile('(.{19});E;(\d+)(?:-(\d+))?\..*;user=(\S+) (?:account=(\S+))?.*group=(\S+).*queue=(\S+) ctime=\d+ qtime=(\d+) etime=(\d+) start=(\d+) .* exec_host=(\S+) .* Resource_List.walltime=(\d+:\d+:\d+) .*\n')
+        self._logpat = re.compile('(.{19});E;(\d+)(?:-(\d+))?\..*;user=(\S+) (?:account=(\S+))?.*group=(\S+).*queue=(\S+) ctime=\d+ qtime=(\d+) etime=(\d+) start=(\d+) .* exec_host=(\S+) .* Exit_status=(\d+) .*\n')
+        self._exechostpat = re.compile('/\d+')
 
         db_section = self._cluster_name + "/torque"
         self._logfolder = config.get(db_section,"logdir")
@@ -42,41 +28,47 @@ class JobImporterTorque(object):
         # No need to update with Torque
         return []
 
-    def get_job_for_id_above(self, id_job):
+    def account_files_to_check(self, last_id_job):
+#        last_filename = last_id_job
+        filenames = os.listdir(self._logfolder)
+        return filenames
+        
+
+    def get_job_for_id_above(self, last_id_job):
         # BETA TEST !!
-        filenames = ["/home/sgorget/clamart2/torque/20120215",]
+        jobs = []
+        filenames = self.account_files_to_check(last_id_job)
         for filename in filenames:
-            accountfile = open(filename,'r')
+            accountfile = open( self._logfolder + "/" + filename,'r')
             for line in accountfile.readlines():
                 match = self._logpat.match(line)
                 if match:
                     jobinfo = match.groups()
-                    print jobinfo[0],jobinfo[3]
-
-
-#        jobs = []
-#        results = self.request_jobs_since_job_id(id_job)
-#        for result in results:
-#            jobs.append(self.job_from_information(result))
-#        return jobs
-        return []
+                    jobs.append(self.job_from_information(jobinfo, filename))
+        return jobs
    
-    def job_from_information(self, res):
-        return []
-        job = Job(  id_job = "id_job",
-                    sched_id = "job_db_inx",
-                    uid = "id_user",
-                    gid = "id_group",
-                    submission_datetime = "time_submit",
-                    running_datetime = "time_start",
-                    end_datetime = "time_end",
-                    nb_procs = "cpus_alloc",
-                    nb_hosts =  "nodes_alloc",
-                    running_queue = "partition",
-                    nodes = "nodelist",
-                    state = "state",
+    def job_from_information(self, res, filename):
+        nbprocs, nbnodes, nodelist = self.torque_job_nodelist(res[10])
+        job = Job(  id_job = int(res[1]),
+                    sched_id = filename,
+                    uid = 0, # Fonction a écrire
+                    gid = 0, # Fonction à écrire
+                    submission_datetime = datetime.fromtimestamp(int(res[7])),
+                    running_datetime = datetime.fromtimestamp(int(res[8])),
+                    end_datetime = datetime.fromtimestamp(int(res[9])),
+                    nb_procs = nbprocs,
+                    nb_hosts = nbnodes,
+                    running_queue = res[6],
+                    nodes = nodelist,
+                    state = self.get_job_state_from_torque_state(res[11]),
                     clustername = self._cluster_name)
         return job
+
+    def get_job_state_from_torque_state(self, state):
+        if int(state)  == 0:
+            return "COMPLETE"
+        else :
+            return "UNKNOWN"
 
 # TO BE MOVED IN ABSTRACT FUNCTION
     def get_last_job_id(self):
@@ -97,5 +89,18 @@ class JobImporterTorque(object):
 
 
     def get_unfinished_job_id(self):
+        # Torque always put in DB finished jobs
         return []
 
+# Useful function
+    def torque_job_status_converter(self):
+        return ""
+
+    def torque_job_nodelist(self,nodelist):
+        nodelist = self._exechostpat.sub('',nodelist)
+        nodelist = nodelist.split('+')
+        nbprocs = len(nodelist)
+        nodelist = str(NodeSet.fromlist(nodelist))
+        nbnodes = len(nodelist)
+        return nbprocs, nbnodes, nodelist
+        
