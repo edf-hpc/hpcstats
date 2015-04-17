4b843ddc0249d43f927cfaa41a110594e9988ed0
Rémi Palancher <remi-externe.palancher@edf.fr>
2015-04-17 16:50:12 +0200

    Add and homogenize license header everywhere

--- lib/HPCStats/Importer/Jobs/JobImporterFactory.py
+++ lib/HPCStats/Importer/Jobs/JobImporterFactory.py
@@ -1,5 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011-2015 EDF SA
+# Contact:
+#       CCN - HPC <dsp-cspit-ccn-hpc@edf.fr>
+#       1, Avenue du General de Gaulle
+#       92140 Clamart
+#
+# Authors: CCN - HPC <dsp-cspit-ccn-hpc@edf.fr>
+#
+# This file is part of HPCStats.
+#
+# HPCStats is free software: you can redistribute in and/or
+# modify it under the terms of the GNU General Public License,
+# version 2, as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with HPCStats. If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# On Calibre systems, the complete text of the GNU General
+# Public License can be found in `/usr/share/common-licenses/GPL'.
 
 import logging
 from HPCStats.Importer.Jobs.JobImporterSlurm import JobImporterSlurm
