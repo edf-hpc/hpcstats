f78ebcc88a99d8b723fef80a2085cb0d516c56a6
Rémi Palancher <remi-externe.palancher@edf.fr>
2015-04-30 09:46:30 +0200

    Sanitize Project model class

--- HPCStats/Model/Project.py
+++ HPCStats/Model/Project.py
@@ -27,14 +27,32 @@
 # On Calibre systems, the complete text of the GNU General
 # Public License can be found in `/usr/share/common-licenses/GPL'.
 
+"""
+Model class for the Project table:
+
+Project(
+  project_id          SERIAL,
+  project_code        character varying(30) NOT NULL,
+  project_description text,
+  sector_key          character varying(5),
+  domain_id           character varying(4),
+  CONSTRAINT Project_pkey PRIMARY KEY (project_id),
+  CONSTRAINT Project_unique UNIQUE (project_code)
+)
+
+"""
+
 import logging
+from HPCStats.Exceptions import HPCStatsDBIntegrityError, HPCStatsRuntimeError
 
 class Project:
-    def __init__(self, name, description, sector=None, domain=None):
 
-        self.name = name
+    def __init__(self, sector, code, description, project_id=None):
+
+        self.project_id = project_id
+        self.code = code
         self.description = description
-        self.domain = domain
+
         self.sector = sector
 
     def __str__(self):
@@ -42,70 +60,94 @@ class Project:
            sector = "unknown"
         else:
            sector = self.sector
-        if self.domain == None:
-           domain = "unknown"
+        return "sector %s [%s]: %s" \
+                 % (self.code,
+                    self.sector.key,
+                    self.description)
+
+    def find(self, db):
+        """Search the Project in the database based on its code. If exactly
+           one project matches in database, set project_id attribute properly
+           and returns its value. If more than one project matches, raises
+           HPCStatsDBIntegrityError. If no project is found, returns None.
+        """
+
+        req = """
+                SELECT project_id
+                  FROM Project
+                 WHERE project_code = %s
+              """
+        params = ( self.code, )
+        cur = db.get_cur()
+        cur.execute(req, params)
+        nb_rows = cur.rowcount
+        if nb_rows == 0:
+            logging.debug("project %s not found in DB" % (str(self)))
+            return None
+        elif nb_rows == 1:
+            raise HPCStatsDBIntegrityError(
+                    "several project_id found in DB for project %s" \
+                      % (str(self)))
         else:
-           domain = self.domain
-        if self.description == None:
-           description = "unknown"
-        else:
-           description = self.description
-        return self.name + " : " + self.sector + " - " + self.domain + " - " + self.description
+            self.project_id = cur.fetchone()[0]
+            logging.debug("project %s found in DB with id %d" \
+                            % (str(self),
+                               self.project_id))
+            return self.project_id
 
     def save(self, db):
+        """Insert Project in database. You must make sure that the Project does
+           not already exist in database yet (typically using Project.find()
+           method else there is a risk of future integrity errors because of
+           duplicated clusters. If project_id attribute is set, it raises
+           HPCStatsRuntimeError.
+        """
+
+        if self.project_id is not None:
+            raise HPCStatsRuntimeError(
+                    "could not insert project %s since already existing in "\
+                    "database" \
+                      % (str(self)))
+
         req = """
-            INSERT INTO projects (
-                              id_sector,
-                              id_domain,
-                              description,
-                              pareo )
-            VALUES (%s, %s, %s, %s);"""
-        datas = (
-            self.sector,
-            self.domain,
-            self.description,
-            self.name )
-        db.get_cur().execute(req, datas)
+                INSERT INTO Project ( project_code,
+                                      project_description,
+                                      sector_key,
+                                      domain_id )
+                VALUES ( %s, %s, %s, %s )
+                RETURNING project_id
+              """
+
+        # domain_key is None if sector has no domain
+        domain_key = getattr(self.sector.domain, 'key', None))
+
+        params = ( self.code,
+                   self.description,
+                   self.sector.key,
+                   domain_key )
+
+        cur = db.get_cur()
+        #print cur.mogrify(req, params)
+        cur.execute(req, params)
+        self.project_id = cur.fetchone()[0]
 
     def update(self, db):
+        """Update Project description field in database. Raises
+           HPCStatsRuntimeError if self.project_id is None.
+        """
+        if self.project_id is None:
+            raise HPCStatsRuntimeError(
+                    "could not update project %s since not found in database" \
+                      % (str(self)))
+
         req = """
-            UPDATE projects SET
-                         sector = %s,
-                         domain = %s,
-                         description = %s
-            WHERE pareo = %s;"""
-        datas = (
-            self.sector,
-            self.domain,
-            self.description,
-            self.name )
-        db.get_cur().execute(req, datas)
+                UPDATE Project
+                   SET project_description = %s
+                 WHERE project_id = %s
+             """
+        params = ( self.description,
+                   self.project_id )
 
-    def already_exist(self, db):
         cur = db.get_cur()
-        cur.execute("SELECT * FROM projects WHERE pareo = %s", (self.name,))
-        nb_rows = cur.rowcount
-        if nb_rows == 1:
-           return True
-        elif nb_rows == 0:
-           return False
-
-    def project_from_name(self, db, name):
-        cur = db.get_cur()
-        cur.execute("SELECT pareo, description, sector, domain FROM projects WHERE pareo = %s", (name,))
-        res = cur.fetchone()
-        self.name = res[0]
-        self.description = res[1]
-        self.sector = res[2]
-        self.domain = res[3]
-
-def delete_projects(db):
-    db.get_cur().execute("ALTER SEQUENCE projects_id_seq RESTART WITH 1;")   
-    db.get_cur().execute("DELETE FROM projects;")
-
-def get_name_id(db, name):
-    cur = db.get_cur()
-    cur.execute("SELECT id_project FROM projects WHERE lower(pareo) = lower(%s)", (name,))
-    results = cur.fetchone()
-    return results[0]
-
+        #print cur.mogrify(req, params)
+        cur.execute(req, params)
