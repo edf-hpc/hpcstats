6b4581f11b46fbd5b7a2bf994115b6f196bfc659
Rémi Palancher <remi-externe.palancher@edf.fr>
2015-05-20 18:07:45 +0200

    New Utils module with decypher function

---
+++ HPCStats/Utils.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011-2015 EDF SA
+# Contact:
+#       CCN - HPC <dsp-cspit-ccn-hpc@edf.fr>
+#       1, Avenue du General de Gaulle
+#       92140 Clamart
+#
+# Authors: CCN - HPC <dsp-cspit-ccn-hpc@edf.fr>
+#
+# This file is part of HPCStats.
+#
+# HPCStats is free software: you can redistribute in and/or
+# modify it under the terms of the GNU General Public License,
+# version 2, as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with HPCStats. If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# On Calibre systems, the complete text of the GNU General
+# Public License can be found in `/usr/share/common-licenses/GPL'.
+
+def decypher(encoded):
+    """Decypher the string in parameter."""
+
+    result = []
+    for coord_i in xrange(len(encoded)):
+        coord_j = ord(encoded[coord_i])
+        if coord_j >= 33 and coord_j <= 126:
+            result.append(chr(33 + ((coord_j + 14) % 94)))
+        else:
+            result.append(encoded[coord_i])
+    return ''.join(result)
