5a695f4652455c3a5e052acab6d154725fdd1a49
Rémi Palancher <remi-externe.palancher@edf.fr>
2015-05-21 17:38:37 +0200

    Sanitize FSUsageImporterSSH class

--- HPCStats/Importer/FSUsage/FSUsageImporterSSH.py
+++ HPCStats/Importer/FSUsage/FSUsageImporterSSH.py
@@ -27,6 +27,7 @@
 # On Calibre systems, the complete text of the GNU General
 # Public License can be found in `/usr/share/common-licenses/GPL'.
 
+from operator import attrgetter
 from datetime import datetime
 import time
 import base64
@@ -46,102 +47,91 @@ class FSUsageImporterSSH(FSUsageImporter):
 
         super(FSUsageImporterSSH, self).__init__(app, db, config, cluster)
 
-        usage_section = self.cluster.name + "/fsusage"
+        section = self.cluster.name + "/fsusage"
 
-        self._fshost = config.get(usage_section,"host")
-        self._fsname = config.get(usage_section,"name")
-        # No need to set self._fspassword if you had delivered public ssh key
-        #self._fspassword = config.get(usage_section,"password")
-        self._fsfile = config.get(usage_section,"file")
-        self._fs_usage = [] #List of usages for a fs
-        self._usage = [] #Output file from ssh connection
+        self.ssh_host = config.get(section, 'host')
+        self.ssh_user = config.get(section, 'user')
+        self.ssh_pkey = config.get(section, 'pkey')
+        self.fsfile = config.get(section, 'file')
+
+        self.filesystems = None # loaded filesystems
+        self.fsusages = None    # loaded fsusages
+
+    def load(self):
+        """Load Filesystems and FSUsages from CSV logfile read through SSH."""
  
+        self.filesystems = []
+        self.fsusages = []
+
         try:
-            logging.info("Start ssh connection on cluster %s", self.cluster.name)
-            #Paramiko is used to initiate ssh connection
-            self._ssh = paramiko.SSHClient()
-            #Set automatically RSA key on known_hosts file
-            self._ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            self._ssh.connect(self._fshost, username=self._fsname)
-            # No need to use password if you had delivered public ssh key
-            #self._ssh.connect(self._fshost, username=self._fsname, \
-            #    password=base64.b64decode( \
-            #    self.decypher(base64.b64decode(self._fspassword))))
-            #Send shell command via ssh
-            self._stdin, self._stdout, self._stderr = self._ssh.exec_command \
-                ("cat %s*" %self._fsfile)
-            self._usage = self._stdout.readlines()
-            #print self._usage
-            #Get filesytem from HPCStats db
-            fs_list = Filesystem().get_fs_for_cluster(self.db, self.cluster)
-            logging.info("Get fs for cluster %s : %s", self.cluster.name, fs_list)
+            logging.debug("ssh connection to %s@%s",
+                          self.ssh_user,
+                          self.ssh_host)
+            ssh = paramiko.SSHClient()
+            # set automatically RSA key on known_hosts file
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(self.ssh_host,
+                        username=self.ssh_user,
+                        key_filename=self.ssh_pkey)
+        except (paramiko.AuthenticationException, socket.error) as err:
+            raise HPCStatsSourceError( \
+                    "unable to connect by SSH to %s@%s: %s" % \
+                      (self.ssh_user, self.ssh_host, err))
 
-            #For all files system on the cluster
-            for fs in fs_list:
-                #Innitiate fs_usage for new fs
-                self._fs_usage = []
-                #Get last fs timestamp from HPCStats db
-                timestamp = self._get_last_fs_timestamp_usage(fs)
-                logging.info("Get %s %s last timestamp : %s", \
-                              self.cluster.name, fs, timestamp )
-                if timestamp:
-                    #Get usage list from cluster
-                    logging.info("Get new %s %s usages", self.cluster.name, fs)
-                    self._get_usage_list(fs, timestamp)
-                #Update HPCStats database with new values
-                if self._fs_usage:
-                    logging.info("List of %s usages to add for cluster %s : %s", \
-                          fs, \
-                          self.cluster.name, \
-                          self._fs_usage)
-                    #for fs_usage in self._fs_usage:
-                    Filesystem_usage().update_usage(self.db, self.cluster, fs, self._fs_usage)
-                else:
-                    logging.info("No new %s %s usages", self.cluster.name, fs)
+        # The remote file is accessed through SFTP. We could have used
+        # Paramiko sftp.open() but iterating over a long file (line by line)
+        # is quite slow. We prefer to download the full file to a local
+        # temporary file and then read/parse this local file.
+        sftp = ssh.open_sftp()
 
+        (tmp_fh, tmp_fpath) = tempfile.mkstemp()
+        tmp_fh.close() # immediately close since we do not need it
 
-        except (paramiko.AuthenticationException, socket.error ) as e:
-            logging.error("ssh connection to cluster %s failed: %s", \
-                           self.cluster.name, e)
-            raise RuntimeError
+        # download file through SFTP
+        sftp.get(self.fsfile, tmp_fpath)
 
+        # close sftp and ssh since we do not need them anymore.
+        sftp.close()
+        ssh.close()
 
-    def _get_usage_list(self, fs, timestamp):
-        for line in self._usage:
-            time_from_line = line.split(',')[1]
-            fs_from_line = line.split(',')[0]
-            time_list = datetime.strptime(time_from_line, '%Y-%m-%d %H:%M:%S')
-            if fs == fs_from_line:
-                if time_list > timestamp:
-                    self._fs_usage.append((line.split(',')[1], line.split(',')[2], line.split(',')[3]))
+        with open(tmp_fpath, 'rb') as csvfile:
+            csvreader = csv.reader(csvfile, delimiter=',')
+            for row in csvreader:
+                mountpoint = row[0]
+                logtime = datetime.strptime("%Y-%m-%dT%H:%M:%S.%fZ", row[1])
+                bpercent = float(row[2])
+                ipercent = float(row[3])
+                newfs = Filesystem(mountpoint, self.cluster)
+                fs = None
+                # search if fs not already defined
+                for xfs in self.filesystems:
+                    if xfs == newfs:
+                        fs = xfs
+                if fs is None:
+                    # newfs not found in defined fs
+                    self.filesystems.append(newfs)
+                    fs = newfs
+                fsusage = FSUsage(fs, logtime, bpercent)
+                self.fsusages.append(fsusage)
 
-    def _get_last_fs_timestamp_usage(self, fs):
-        last_usage_timestamp = 0
-        req = """
-            SELECT MAX(timestamp) AS last_usage
-            FROM filesystem_usage, filesystem
-            WHERE filesystem_usage.fs_id=filesystem.id
-            AND filesystem.cluster = %s
-            AND filesystem.mount_point = %s
-              ; """
-        datas = (self.cluster.name,
-                 fs,)
-        cur = self.db.cur
-        cur.execute(req, datas)
-        if cur.fetchone()[0] is None:
-            result = datetime(1970, 1, 1, 0, 0)
-        else:
-            cur.execute(req, datas)
-            result = cur.fetchone()[0]
-        #logging.info("Get %s %s timestamp : %s", self.cluster.name, fs, result )
-        return result
+        # sort fsusages by datetime in asc. order
+        self.fsusages.sort(key=attrgetter('datetime'))
 
-    def decypher(self, s):
-        x = []
-        for i in xrange(len(s)):
-            j = ord(s[i])
-            if j >= 33 and j <= 126:
-                x.append(chr(33 + ((j + 14) % 94)))
-            else:
-                x.append(s[i])
-        return ''.join(x)
+    def update(self):
+        """Update Filesystems and FSUsage in DB."""
+
+        last_fs_usage_datetimes = dict()
+
+        for fs in self.filesystems:
+            # There is nothing to udpate for filesystem so just save them
+            # if they do not exist in DB.
+            if not fs.find(self.db):
+                fs.save(self.db)
+            last_fs_usage_datetimes[fs.mountpoint] = \
+              get_last_fsusage_datetime(self.db, self.cluster, fs)
+
+        for fsusage in self.fsusages:
+            # save only new fsusag, ie. with datetime over last datetime in DB
+            if fsusage.datetime > \
+               last_fs_usage_datetimes[fsusage.filesystem.mountpoint]:
+                fsusage.save()
