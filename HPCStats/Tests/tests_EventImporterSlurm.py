1818fcb74df87e3072f618b9aaf9595427ecbf34
Rémi Palancher <remi-externe.palancher@edf.fr>
2015-05-28 17:27:13 +0200

    Improve tests of EventImporterSlurm with loaded data

--- HPCStats/Tests/tests_EventImporterSlurm.py
+++ HPCStats/Tests/tests_EventImporterSlurm.py
@@ -27,9 +27,12 @@
 # On Calibre systems, the complete text of the GNU General
 # Public License can be found in `/usr/share/common-licenses/GPL'.
 
+from datetime import datetime
+import time
 import mock
 
 from HPCStats.Model.Cluster import Cluster
+from HPCStats.Model.Node import Node
 from HPCStats.Importer.Events.EventImporterSlurm import EventImporterSlurm
 from HPCStats.DB.HPCStatsDB import HPCStatsDB
 from HPCStats.Conf.HPCStatsConf import HPCStatsConf
@@ -68,9 +71,9 @@ class TestsEventImporterSlurm(HPCStatsTestCase):
         HPCStatsConf.__bases__ = (MockConfigParser, object)
         self.conf = HPCStatsConf(self.filename, self.cluster)
         self.conf.conf = CONFIG.copy()
-        self.app = MockApp()
         self.db = HPCStatsDB(self.conf)
         self.db.bind()
+        self.app = MockApp(self.db, self.conf, self.cluster.name)
         self.importer = EventImporterSlurm(self.app,
                                            self.db,
                                            self.conf,
@@ -85,7 +88,36 @@ class TestsEventImporterSlurm(HPCStatsTestCase):
                 mock_mysqldb())
     def test_init(self):
         """EventImporterSlurm.load() works with simple data."""
-        MockMySQLdb.MY_REQS = {}
+
+        e1_start = datetime(2015, 3, 2, 15, 59, 59)
+        e1_end = datetime(2015, 3, 2, 16, 0, 0)
+        e1_start_ts = time.mktime(e1_start.timetuple())
+        e1_end_ts = time.mktime(e1_end.timetuple())
+
+        node_name = 'node1'
+        MockMySQLdb.MY_REQS = {
+          'get_events': {
+            'req': "SELECT time_start, time_end, node_name, cpu_count, " \
+                          "state, reason " \
+                   "FROM .*_event_table " \
+                   "WHERE node_name <> '' " \
+                   "AND time_start >= UNIX_TIMESTAMP(.*) " \
+                   "ORDER BY time_start",
+            'res': [
+              [ e1_start_ts, e1_end_ts,
+                node_name, 16, 35, 'reason1' ],
+            ]
+          }
+        }
+
+        self.app.arch.nodes = [ Node(node_name, self.cluster, 'partition1', 16, 8, 0), ]
         self.importer.load()
+        self.assertEquals(1, len(self.importer.events))
+        event = self.importer.events[0]
+        self.assertEquals(event.start_datetime, e1_start)
+        self.assertEquals(event.end_datetime, e1_end)
+        self.assertEquals(event.nb_cpu, 16)
+        self.assertEquals(event.event_type, 'ALLOCATED+RES')
+        self.assertEquals(event.reason, 'reason1')
 
 loadtestcase(TestsEventImporterSlurm)
