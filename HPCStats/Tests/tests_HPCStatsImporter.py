2c85cc70ac1fbc1ab18433dfb50df6c55197907a
Rémi Palancher <remi-externe.palancher@edf.fr>
2016-02-12 13:56:31 +0100

    Implement HPCStatsImporter.run() unit test

--- HPCStats/Tests/tests_HPCStatsImporter.py
+++ HPCStats/Tests/tests_HPCStatsImporter.py
@@ -31,8 +31,11 @@ import mock
 from HPCStats.Exceptions import *
 from HPCStats.CLI.HPCStatsImporter import HPCStatsImporter
 from HPCStats.Conf.HPCStatsConf import HPCStatsConf
+from HPCStats.Model.Cluster import Cluster
 from HPCStats.Tests.Mocks.MockConfigParser import MockConfigParser
 from HPCStats.Tests.Utils import HPCStatsTestCase, loadtestcase
+import HPCStats.Tests.Mocks.MockPg2 as MockPg2 # for PG_REQS
+from HPCStats.Tests.Mocks.MockPg2 import mock_psycopg2, init_reqs
 
 CONFIG = { 'clusters': {
              'clusters': 'testcluster,testcluster2',
@@ -44,11 +47,12 @@ CONFIG = { 'clusters': {
              'user':     'test_user',
              'password': 'test_password',
            },
-           'globals': {
-             'projects': 'csv',
-           },
-           'projects': {
-             'file': 'fake_project_file',
+           'testcluster': {
+             'architecture': 'archfile',
+             'users': 'ldap',
+             'fsusage': 'ssh',
+             'events': 'slurm',
+             'jobs': 'slurm',
            },
            'testcluster/slurm': {
              'host': 'dbhost',
@@ -68,17 +72,43 @@ class TestsHPCStatsImporter(HPCStatsTestCase):
         self.conf = HPCStatsConf(self.filename, self.cluster)
         self.conf.conf = CONFIG.copy()
         self.importer = HPCStatsImporter(self.conf, self.cluster)
+        init_reqs()
 
     def test_init(self):
         """HPCStatsImporter.__init__() run w/o problem
         """
         pass
 
-    def test_run(self):
-        """HPCStatsImporter.run() NOT SUPPOSED TO WORK YET.
+    @mock.patch("HPCStats.DB.HPCStatsDB.psycopg2", mock_psycopg2())
+    @mock.patch("HPCStats.Importer.Jobs.JobImporterFactory.JobImporterSlurm")
+    @mock.patch("HPCStats.Importer.Events.EventImporterFactory.EventImporterSlurm")
+    @mock.patch("HPCStats.Importer.FSUsage.FSUsageImporterFactory.FSUsageImporterSSH")
+    @mock.patch("HPCStats.Importer.Users.UserImporterFactory.UserImporterLdap")
+    @mock.patch("HPCStats.Importer.Architectures.ArchitectureImporterFactory.ArchitectureImporterArchfile")
+    def test_run(self, arch_m, user_m, fs_m, event_m, job_m):
+        """HPCStatsImporter.run() calls all importer load/update methods.
         """
-        pass
-        #self.importer.run()
+
+        # Get mocked classes instances, and configure the cluster attribute of
+        # ArchImporter instance
+        arch_i = arch_m.return_value
+        arch_i.cluster = Cluster(self.cluster, 0)
+        user_i = user_m.return_value
+        fs_i = fs_m.return_value
+        event_i = event_m.return_value
+        job_i = job_m.return_value
+
+        self.importer.run()
+
+        arch_i.load.assert_called_once_with()
+        arch_i.update.assert_called_once_with()
+        user_i.load.assert_called_once_with()
+        user_i.update.assert_called_once_with()
+        fs_i.load.assert_called_once_with()
+        fs_i.update.assert_called_once_with()
+        event_i.load.assert_called_once_with()
+        event_i.update.assert_called_once_with()
+        job_i.load_update_window.assert_called_once_with()
 
     def test_run_exception_no_hpcstatsdb(self):
         """HPCStatsImporter.run() raise exception when hpcstatsdb section is
